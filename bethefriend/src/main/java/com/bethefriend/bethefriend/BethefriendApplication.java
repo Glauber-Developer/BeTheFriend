7af7c8bc92eab1194ffa80787f85423e97f038d3
Glauber-Developer <glauber.araujo@edu.pucrs.br>
2024-12-06 16:09:40 -0300

    chore: first commit

---
+++ bethefriend/src/main/java/com/bethefriend/bethefriend/BethefriendApplication.java
@@ -0,0 +1,13 @@
+package com.bethefriend.bethefriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethefriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethefriendApplication.class, args);
+	}
+
+}
---
+++ bethefriend/src/test/java/com/bethefriend/bethefriend/BethefriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bethefriend.bethefriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethefriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
