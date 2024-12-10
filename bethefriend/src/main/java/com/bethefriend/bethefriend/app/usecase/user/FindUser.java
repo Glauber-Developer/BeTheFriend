983ed9ed949d6ba646bb474eea10d89e8f0fde4e
Glauber-Developer <glauber.araujo@edu.pucrs.br>
2024-12-10 12:38:49 -0300

    feat(userservices): add FindUser #11

---
+++ bethefriend/src/main/java/com/bethefriend/bethefriend/app/usecase/user/FindUser.java
@@ -0,0 +1,33 @@
+package com.bethefriend.bethefriend.app.usecase.user;
+
+import org.springframework.stereotype.Service;
+
+import com.bethefriend.bethefriend.domain.user.User;
+import com.bethefriend.bethefriend.domain.user.UserType;
+import com.bethefriend.bethefriend.infrastructure.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FindUser {
+
+    private final UserRepository userRepository;
+
+    public FindUser(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public List<User> getUsersByType(UserType type) {
+        return userRepository.findByType(type);
+    }
+}
