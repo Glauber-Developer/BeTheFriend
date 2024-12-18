ae1aa7abc9dfa6e562577dd515a34c6fbfcbf6c6
Glauber-Developer <glauber.araujo@edu.pucrs.br>
2024-12-18 16:16:17 -0300

    feat(userDTO): add UserDTO #137

---
+++ bethefriend/src/main/java/com/bethefriend/bethefriend/domain/user/UserDTO.java
@@ -0,0 +1,12 @@
+package com.bethefriend.bethefriend.domain.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String name;
+    
+}
