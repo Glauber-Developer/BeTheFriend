df003d5de365caf1df04629691903cddacff0fa0
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2024-12-17 23:35:48 -0300

    feat (MyProfile) : add status activities button #126

--- bethefriend/src/main/java/com/bethefriend/bethefriend/app/controller/ActivityController.java
+++ bethefriend/src/main/java/com/bethefriend/bethefriend/app/controller/ActivityController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.*;
 import com.bethefriend.bethefriend.app.usecase.activity.CreateActivity;
 import com.bethefriend.bethefriend.app.usecase.activity.FindActivityByUser;
 import com.bethefriend.bethefriend.app.usecase.activity.FindAllActivities;
+import com.bethefriend.bethefriend.app.usecase.activity.UpdateActivityByUser;
 import com.bethefriend.bethefriend.domain.Activity;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/activities")
@@ -18,20 +23,28 @@ public class ActivityController {
     private final CreateActivity createActivityUseCase;
     private final FindAllActivities findAllActivitiesUseCase;
     private final FindActivityByUser findActivitiesByUserUseCase;
+    private final UpdateActivityByUser updateActivitiesByUserUseCase;
 
     public ActivityController(
         CreateActivity createActivityUseCase,
         FindAllActivities findAllActivitiesUseCase,
-        FindActivityByUser findActivitiesByUserUseCase) {
+        FindActivityByUser findActivitiesByUserUseCase, 
+        UpdateActivityByUser updateActivitiesByUserUseCase) {
         this.createActivityUseCase = createActivityUseCase;
         this.findAllActivitiesUseCase = findAllActivitiesUseCase;
         this.findActivitiesByUserUseCase = findActivitiesByUserUseCase;
+        this.updateActivitiesByUserUseCase = updateActivitiesByUserUseCase;
     }
 
     @PostMapping
     public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
         Activity newActivity = createActivityUseCase.createActivity(activity);
         return ResponseEntity.status(HttpStatus.CREATED).body(newActivity);
+    }
+    @PutMapping("/{userId}")
+    public ResponseEntity<Activity> updateActivity(@PathVariable Long userId, @RequestBody Activity activity) {
+        Activity updateActivity = updateActivitiesByUserUseCase.updateActivity(userId, activity);       
+        return ResponseEntity.status(HttpStatus.OK).body(updateActivity);
     }
 
     @GetMapping
